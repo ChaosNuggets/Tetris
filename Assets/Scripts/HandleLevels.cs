c648716d5075a5197201aa1eb6dfba96ed79379d
ChaosNuggets <46007806+ChaosNuggets@users.noreply.github.com>
2022-08-28 21:52:54 -0400

    made pieces fall faster as levels increase

--- Assets/Scripts/HandleLevels.cs
+++ Assets/Scripts/HandleLevels.cs
@@ -12,16 +12,54 @@ public class HandleLevels : MonoBehaviour
     public GameObject canvas;
     private static HandleText ht;
 
+
     private void OnEnable()
     {
         ht = canvas.GetComponent<HandleText>();
     }
 
+    private static float calculateDropTime(int level)
+    {
+        float[] droptimes =
+        {
+            48f / 60f,
+            43f / 60f,
+            38f / 60f,
+            33f / 60f,
+            28f / 60f,
+            23f / 60f,
+            18f / 60f,
+            13f / 60f,
+            8f / 60f,
+            6f / 60f,
+            5f / 60f,
+            5f / 60f,
+            5f / 60f,
+            4f / 60f,
+            4f / 60f,
+            4f / 60f,
+            3f / 60f,
+            3f / 60f,
+            3f / 60f
+        };
+        return level < 19 ? droptimes[level] :
+            level < 29 ? 2f / 60f : 1f / 60f;
+    }
+
     public static void setStartLevel(int level)
     {
         currentLevel = level;
-        transitionLines = level <= 9 ? (level + 1) * 10 : (level >= 16 ? (level - 5) * 10 : 100);
-        prevDivBy10 = transitionLines / 10;
+        transitionLines = level <= 9 ? (level + 1) * 10 :
+            level >= 16 ? (level - 5) * 10 : 100;
+        MovePieces.regularDropTime = calculateDropTime(currentLevel);
+    }
+
+    private static void incrementLevel(int currentDivBy10)
+    {
+        prevDivBy10 = currentDivBy10;
+        currentLevel++;
+        ht.updateLevelText(currentLevel);
+        MovePieces.regularDropTime = calculateDropTime(currentLevel);
     }
     
     public static void addLineClear()
@@ -33,14 +71,11 @@ public class HandleLevels : MonoBehaviour
             if (linesCleared >= transitionLines)
             {
                 isOnFirstLevel = false;
-                currentLevel++;
-                ht.updateLevelText(currentLevel);
+                incrementLevel(currentDivBy10);
             }
         } else if (currentDivBy10 > prevDivBy10)
         {
-            prevDivBy10 = currentDivBy10;
-            currentLevel++;
-            ht.updateLevelText(currentLevel);
+            incrementLevel(currentDivBy10);
         }
         Debug.Log(linesCleared);
     }
